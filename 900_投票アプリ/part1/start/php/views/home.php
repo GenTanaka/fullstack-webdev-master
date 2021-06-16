a6687d8baffb77182655047ba1ef1ab243e60589
github GenTanaka <gen.tanaka2001@gmail.com>
2021-06-17 01:21:00 +0900

    6/17 進捗状況
    6/17までに進んだ文の追加

---
+++ 900_投票アプリ/part1/start/php/views/home.php
@@ -0,0 +1,6 @@
+
+	<h1>TOPページ</h1>
+	<!-- <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg" alt=""> -->
+<form action="<?php BASE_CONTEXT_PATH; ?>login" method="post">
+	<input type="submit" value="送信">
+</form>
